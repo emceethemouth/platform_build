9cce4a5e07125f63e769c204c1b9038ef7065344
kiko <jfdsmabalot@gmail.com>
2014-06-25 13:34:56 +0800

    script: Odex build script Build HH WITH_DEXPREOPT=true
    Signed-off-by: Jean Francis Dominique S. Mabalot <jfdsmabalot@redhat.com>

---
+++ hammerhead-odex.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Android Open Source Project
+#
+# Rom script builder for AOSP build
+#
+# Created by: Jean Francis Dominique S. Mabalot (Gnome)
+# Copyright 2014
+#
+
+echo "Start Compilation for Hammerhead"
+echo ""
+echo ""
+
+
+echo "cleaning directories..."
+make clean
+clear
+
+echo "start"
+cd ..
+. build/envsetup.sh
+lunch 9
+echo ""
+echo "Building Hammerhead Odex"
+echo ""
+make otapackage -j4 WITH_DEXPREOPT=true 2>&1 | tee build.log
+
+echo "build done"
+echo ""
+echo ""
+echo "If ever you encounter any errors, just check the build.log"
+echo ""
+echo ""
+echo "Script done by Gnome@xda-developers.com"
+echo ""
+echo ""
